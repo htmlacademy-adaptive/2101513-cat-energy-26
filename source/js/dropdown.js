e3149b17c7b2807d76fd73df04f4a3e5b18877fa
Artur I <ilyachever@gmail.com>
2022-08-21 19:01:57 +0400

    Дополнение к предыдущему коммиту

--- source/js/dropdown.js
+++ source/js/dropdown.js
@@ -1,6 +1,11 @@
 let navigation = document.querySelector('.main-nav');
+let header = document.querySelector('.main-header__container');
 let button = document.querySelector('.main-header__nav-toggle');
 
+navigation.classList.remove('main-nav--nojs');
+header.classList.remove('main-header__container--nojs');
+button.classList.remove('main-header__nav-toggle--nojs');
+
 button.onclick = function() {
-  navigation.classList.toggle('main-nav--close');
+  navigation.classList.toggle('main-nav--closed');
 }
