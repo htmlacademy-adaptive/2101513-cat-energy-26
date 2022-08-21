47460c8822c8f5837bb8de34f451fc91792d5006
Artur I <ilyachever@gmail.com>
2022-08-21 18:49:52 +0400

    Добавил состояния при выключенном JS для навигации

--- source/js/dropdown.js
+++ source/js/dropdown.js
@@ -1,6 +1,6 @@
-let nav = document.querySelector('.main-header__nav');
+let navigation = document.querySelector('.main-nav');
 let button = document.querySelector('.main-header__nav-toggle');
 
 button.onclick = function() {
-  nav.classList.toggle('main-nav--close');
+  navigation.classList.toggle('main-nav--close');
 }
