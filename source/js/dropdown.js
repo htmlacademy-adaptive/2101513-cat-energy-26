30584f805a32b5b37f9df36451976931f8d99df8
Artur I <ilyachever@gmail.com>
2022-09-14 10:39:07 +0400

    Правки всех страниц

--- source/js/dropdown.js
+++ source/js/dropdown.js
@@ -8,4 +8,11 @@ button.classList.remove('main-header__nav-toggle--nojs');
 
 button.onclick = function() {
   navigation.classList.toggle('main-nav--closed');
+  this.classList.toggle('main-header__nav-toggle--closed');
+  if (this.classList.contains('main-header__nav-toggle--closed')) {
+    this.classList.remove('main-header__nav-toggle--opened');
+  }
+  else {
+    this.classList.add('main-header__nav-toggle--opened');
+  }
 }
